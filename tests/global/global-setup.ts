1769db5a90835bb392aa6385a76abdc0e26e270c
Zoltan Timar <zoltan.timar.work@gmail.com>
2026-09-22 12:47:30 +0200

    chore: moved standard_user creds to .env

--- tests/global/global-setup.ts
+++ tests/global/global-setup.ts
@@ -3,22 +3,20 @@ import path from 'path';
 
 import { chromium, type FullConfig } from '@playwright/test';
 
-import type { Credentials } from '../../types/data';
-
 export const authFile = path.resolve(process.cwd(), 'playwright/.auth/standard_user.json');
 
-export default async function globalSetup(config: FullConfig) {
-  const baseURL = config.projects[0]?.use?.baseURL;
-  const { usernames, password }: Credentials = JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), 'data/credentials.json'), 'utf-8'),
-  );
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default async function globalSetup(_: FullConfig) {
+  const baseURL = process.env.BASE_URL;
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
 
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
   await page.goto(baseURL!);
-  await page.locator('#user-name').fill(usernames[0]);
-  await page.locator('#password').fill(password);
+  await page.locator('#user-name').fill(username!);
+  await page.locator('#password').fill(password!);
   await page.locator('#login-button').click();
   await page.waitForURL(/inventory/);
 
--- types/data.d.ts
+++
@@ -1,4 +0,0 @@
-export type Credentials = {
-  usernames: string[];
-  password: string;
-};
